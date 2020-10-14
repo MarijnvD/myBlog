f7d23f87cd9dae25d7320ee54cce50e40caf9da7
Marijn van Dorp <marijnvandorp@gmail.com>
2020-10-14 23:02:33 +0200

    Initial commit blog

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+                
+        public DbSet<Post> Posts{ get; set; }  //Migration tool neemt deze mee en maakt de tabel aan
+
+    }
+
+ }
